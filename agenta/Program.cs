ca7477c883d0dd25444e6599a16f87d82327fc5e
Mehmet İlkman <infoilkman@gmail.com>
2025-06-08 15:09:36 +0300

    Wrote the agent A for reading the files

--- agenta/Program.cs
+++ agenta/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("AgentA running");
+        Console.Write("Please give the path for the txt files folder: ");
+        string directoryPath = Console.ReadLine();
+
+        //checking if the path is correct and exitts
+        if (!Directory.Exists(directoryPath))
+        {
+            Console.WriteLine("Error path is wrong");
+            return;
+        }
+
+        var indexedData = new Dictionary<string, Dictionary<string, int>>();
+
+        string[] files = Directory.GetFiles(directoryPath, "*.txt");
+        foreach (var file in files)
+        {
+            var wordCounts = new Dictionary<string, int>();
+            string content = File.ReadAllText(file);
+
+            // extract the words
+            var words = Regex.Matches(content.ToLower(), @"\b\w+\b");
+
+            foreach (Match word in words)
+            {
+                if (wordCounts.ContainsKey(word.Value))
+                    wordCounts[word.Value]++;
+                else
+                    wordCounts[word.Value] = 1;
+            }
+
+            indexedData[Path.GetFileName(file)] = wordCounts;
+        }
+
+        // test output
+        foreach (var fileEntry in indexedData)
+        {
+            foreach (var wordEntry in fileEntry.Value)
+            {
+                Console.WriteLine($"{fileEntry.Key}:{wordEntry.Key}:{wordEntry.Value}");
+            }
+        }
+
+        Console.WriteLine("Press any key to close");
+        Console.ReadKey();
+    }
+}
