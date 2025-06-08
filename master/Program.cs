edc816bf22333f80d9051d82c887dfe44288877d
Mehmet İlkman <infoilkman@gmail.com>
2025-06-08 18:53:17 +0300

    Multithreading in master, added agent B, polished master

--- master/Program.cs
+++ master/Program.cs
@@ -1,33 +1,26 @@
 ﻿using System.IO.Pipes;
 using System.Text.Json;
+using System.Collections.Concurrent;
 
 class Program
 {
+    static ConcurrentDictionary<string, Dictionary<string, int>> finalData = new();
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Master running searching for agent A");
+        Console.WriteLine("Master running searching for agents...");
 
-        using NamedPipeServerStream pipeServer = new("agent1pipe", PipeDirection.In);
-        pipeServer.WaitForConnection();
+        Thread agentAThread = new(() => ReceiveFromAgent("agent1pipe"));
+        Thread agentBThread = new(() => ReceiveFromAgent("agent2pipe"));
 
-        using StreamReader reader = new(pipeServer);
-        string json = reader.ReadLine();
+        agentAThread.Start();
+        agentBThread.Start();
 
-        if (json == null)
-        {
-            Console.WriteLine("No data");
-            return;
-        }
+        agentAThread.Join();
+        agentBThread.Join();
 
-        var data = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, int>>>(json);
-
-        if (data == null)
-        {
-            Console.WriteLine("Deserialization error");
-            return;
-        }
-
-        foreach (var fileEntry in data)
+     
+        foreach (var fileEntry in finalData)
         {
             foreach (var wordEntry in fileEntry.Value)
             {
@@ -37,5 +30,37 @@ class Program
 
         Console.WriteLine("Done receiveing data");
         Console.ReadKey();
+    }
+
+    static void ReceiveFromAgent(string pipeName)
+    {
+        try
+        {
+            using NamedPipeServerStream pipeServer = new(pipeName, PipeDirection.In);
+            Console.WriteLine($"Waiting for {pipeName}");
+            pipeServer.WaitForConnection();
+
+            using StreamReader reader = new(pipeServer);
+            string json = reader.ReadLine();
+
+            if (json != null)
+            {
+                var data = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, int>>>(json);
+
+                if (data != null)
+                {
+                    foreach (var file in data)
+                    {
+                        finalData[file.Key] = file.Value;
+                    }
+
+                    Console.WriteLine($"Received data from {pipeName}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"got Error from {pipeName}: {ex.Message}");
+        }
     }
 }
